6b01a1b6fd089c24d7e1dce72e26506b053a7288
Alessandro Bispo dos Santos <alessandro.santos@PROWAY.TREINA>
2022-07-16 14:56:37 -0300

    Exercicio aula 02

--- 20220709_aula02/ex03.py
+++ 20220709_aula02/ex03.py
@@ -29,6 +29,21 @@ if __name__ == "__main__":
 
     lista_de_itens = []
 
+    produto_com_maior_quantidade = {
+        "nome": "", "quantidade": 0, "valor": 0
+    }
+
+    produto_com_maior_preco = {
+        "nome": "", "quantidade": 0, "valor": 0
+    }
+
+    produto_com_maior_valor = {
+        "nome": "", "valor": 0
+    }
+
+    total_de_itens_pedidos = 0
+    total_geral = 0
+
     for i in range(10):
 
         nome_item = input("Informe o nome do item: ")
@@ -43,4 +58,48 @@ if __name__ == "__main__":
 
         lista_de_itens.append(info)
 
-    print(lista_de_itens)
+        # Verificar se esse item tem uma quantidade unitária maior que o anterior(registrado)
+        if info.get("quantidade") > produto_com_maior_quantidade.get("quantidade"):
+            produto_com_maior_quantidade.update(info)
+
+        # Verificar se esse item tem um preço unitário maior que o anterior(registrado)
+        if info.get("valor") > produto_com_maior_preco.get("valor"):
+            produto_com_maior_preco.update(info)
+
+        # Vericar se a quantidade * valor desse item é maior que o registrado.
+        # valor_atual = info.get("quantidade") * info.get("valor")
+        if (info.get("quantidade") * info.get("valor")) > produto_com_maior_valor.get("valor"):
+            produto_com_maior_valor.update({
+                "nome": info.get("nome"),
+                "valor": info.get("quantidade") * info.get("valor")
+            })
+
+        # total_de_itens_pedidos += info.get("quantidade")
+        total_de_itens_pedidos = total_de_itens_pedidos + info.get("quantidade")
+
+        total_geral = total_geral + (info.get("quantidade") * info.get("valor"))
+
+        print("-"*20)
+
+    print(f"Quantidade de itens cadastrados: {len(lista_de_itens)}")
+    print(
+        f"Produto com maior quantidade: "
+        f"{produto_com_maior_quantidade.get('nome')}"
+        f" ({produto_com_maior_quantidade.get('quantidade')})"
+    )
+
+    print(
+        f"Produto com maior preço: "
+        f"{produto_com_maior_preco.get('nome')}"
+        f" ({produto_com_maior_preco.get('valor')})"
+    )
+
+    print(
+        f"Qual foi o maior pedido: "
+        f"{produto_com_maior_valor.get('nome')}"
+        f" ({produto_com_maior_valor.get('valor')})"
+    )
+
+    print(f"Quantidade total de itens: {total_de_itens_pedidos}")
+
+    print(f"Total geral: {total_geral}")
