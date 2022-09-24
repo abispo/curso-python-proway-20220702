ab61a980c9e76ad985174a18ae7aa159bc46796e
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-09-24 14:19:45 -0300

    Arquivos aula 02 web

--- modulo003-python-web/20220917_aula02/store/models.py
+++ modulo003-python-web/20220917_aula02/store/models.py
@@ -5,8 +5,14 @@ class ItemsList(models.Model):
     name = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
 
+    def __str__(self):
+        return self.name
+
 
 class Item(models.Model):
     item_list = models.ForeignKey(ItemsList, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     price = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
--- modulo003-python-web/20220917_aula02/store/views.py
+++ modulo003-python-web/20220917_aula02/store/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import ItemsList
 
 
 def index(request):
-    pass
+
+    all_items_list = ItemsList.objects.all()
+    context = {"all_items_list": all_items_list}
+    return render(request, "store/index.html", context)
 
 
-def detail(request):
-    pass
+def detail(request, item_list_id):
+
+    items_list = get_object_or_404(ItemsList, pk=item_list_id)
+    context = {"items_list": items_list}
+    return render(request, "store/detail.html", context)
