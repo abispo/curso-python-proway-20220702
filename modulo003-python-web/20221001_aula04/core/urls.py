73219f35436530684f3c4b2a6c2f820e208cf5c6
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-10-01 18:22:26 -0300

    Arquivos da aula 04 web

--- modulo003-python-web/20221001_aula04/core/urls.py
+++ modulo003-python-web/20221001_aula04/core/urls.py
@@ -6,4 +6,6 @@ app_name = "core"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:year>/", views.rounds_list, name="rounds_list"),
+    path("<int:year>/<int:round_id>", views.round_detail, name="round_detail")
 ]
--- modulo003-python-web/20221001_aula04/core/views.py
+++ modulo003-python-web/20221001_aula04/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Round
+from .models import Round, Match
 
 
 def index(request):
@@ -10,3 +10,20 @@ def index(request):
     years.sort()
 
     return render(request, "core/index.html", {"years": years})
+
+
+def rounds_list(request, year):
+
+    all_rounds = Round.objects.filter(year=year).order_by("round_number")
+
+    return render(request, "core/rounds_list.html", {"all_rounds": all_rounds})
+
+
+def round_detail(request, year, round_id):
+    round = Round.objects.get(pk=round_id)
+    all_matches = Match.objects.filter(round=round)
+
+    return render(
+        request,
+        "core/round_detail.html", {"round": round, "all_matches": all_matches})
+
