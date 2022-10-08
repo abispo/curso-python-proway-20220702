728936d72b395830ea7cb19ae7a574a2e9fe4ab2
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-10-08 18:02:52 -0300

    Arquivos aula 05 web

--- modulo003-python-web/20221008_aula05/catalogo/urls.py
+++ modulo003-python-web/20221008_aula05/catalogo/urls.py
@@ -1,4 +1,9 @@
+from django.urls import path
+from . import views
+
+
+app_name = "catalogo"
 
 urlpatterns = [
-
+    path("", views.index, name="index")
 ]
--- modulo003-python-web/20221008_aula05/catalogo/views.py
+++ modulo003-python-web/20221008_aula05/catalogo/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Livro, CopiaLivro, Autor
+
+
+def index(request):
+
+    qtd_livros = Livro.objects.count()
+    qtd_copias = CopiaLivro.objects.count()
+
+    # filter -> WHERE
+    qtd_copias_disponiveis = CopiaLivro.objects.filter(
+        status__exact='d'
+    ).count()
+
+    qtd_autores = Autor.objects.count()
+
+    context = {
+        'qtd_livros': qtd_livros,
+        'qtd_copias': qtd_copias,
+        'qtd_copias_disponiveis': qtd_copias_disponiveis,
+        'qtd_autores': qtd_autores
+    }
+
+    return render(request, 'index.html', context=context)
