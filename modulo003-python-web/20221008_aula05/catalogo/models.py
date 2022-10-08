f0abb0e735cc4944860cab1b75b9b83c35261469
Alessandro Bispo dos Santos <alessandro.santos@PROWAY.TREINA>
2022-10-08 16:13:01 -0300

    Arquivos aula 05 web

--- modulo003-python-web/20221008_aula05/catalogo/models.py
+++ modulo003-python-web/20221008_aula05/catalogo/models.py
@@ -1,3 +1,81 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.urls import reverse
+
+
+class Genero(models.Model):
+
+    # help_text é o texto que aparecerá no admin do Django
+    nome = models.CharField(max_length=200, help_text="Informe um gênero literário (Ficção Científica, Terror, etc)")
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        db_table = 'tb_generos'
+
+
+class Livro(models.Model):
+
+    titulo = models.CharField(max_length=200)
+    autor = models.ForeignKey('Autor', on_delete=models.SET_NULL, null=True)
+    sumario = models.TextField(max_length=100, help_text="Informe uma breve descrição do livro")
+    isbn = models.CharField("ISBN", max_length=13, help_text="Informe o ISBN (13 caracteres)")
+    genero = models.ManyToManyField(Genero, help_text="Informe um gênero para esse livro")
+
+    def __str__(self):
+        return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('detalhe-livro', args=[str(self.id)])
+
+    class Meta:
+        db_table = 'tb_livros'
+
+
+class CopiaLivro(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="ID da cópia")
+    livro = models.ForeignKey(Livro, on_delete=models.RESTRICT, null=True)
+    impressao = models.CharField(max_length=200)
+    devolucao = models.DateField(null=True, blank=True)
+
+    STATUS_COPIA = (
+        ('m', 'Manutenção'),
+        ('e', 'Emprestado'),
+        ('d', 'Disponível'),
+        ('r', 'Reservado'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=STATUS_COPIA,
+        blank=True,
+        default='m',
+        help_text="Disponibilidade da cópia"
+    )
+
+    def __str__(self):
+        return f'{self.id} ({self.livro.titulo})'
+
+    class Meta:
+        db_table = 'tb_copias'
+        ordering = ['devolucao']
+
+
+class Autor(models.Model):
+
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=100)
+    data_de_nascimento = models.DateField("\u2605", null=True, blank=True)
+    data_de_falecimento = models.DateField("\u271F", null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.sobrenome}, {self.nome}'
+
+    def get_absolute_url(self):
+        return reverse('detalhe-autor', args=[str(self.id)])
+
+    class Meta:
+        ordering = ['sobrenome', 'nome']
