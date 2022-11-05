88fc419301fe2ce67610af5cb1fde713d20092a0
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-11-05 18:32:45 -0300

    Arquivos aula 09 web

--- modulo003-python-web/20221105_aula09/02_testes_unitarios/mensagens/tests/test_models.py
+++ modulo003-python-web/20221105_aula09/02_testes_unitarios/mensagens/tests/test_models.py
@@ -5,9 +5,14 @@ from mensagens.models import Mensagem
 
 class TestMensagem(TestCase):
 
+    @classmethod
+    def setUpTestData(cls):
+        cls.postit = Mensagem(titulo="Cachorro quente sabor chucrute", corpo="Sem comentários", autor="Joe").save()
+
     def test_slug_titulo_deve_retornar_titulo_slugificado(self):
-        postit = Mensagem(titulo="Cachorro quente sabor chucrute", corpo="Sem comentários", autor="Joe").save()
+
+        mensagem = Mensagem.objects.first()
 
         self.assertEqual(
-            postit.slug_titulo(), 'cachorro-quente-sabor-chucrute'
+            mensagem.slug_titulo(), 'cachorro-quente-sabor-chucrute'
         )
