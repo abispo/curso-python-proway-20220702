53c23e1bbb1c0490c42a42ab2b120f0d7a5cdbbf
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-11-12 16:41:44 -0300

    Arquivos aula 10 web

--- modulo003-python-web/20221112_aula10/mensagens/tests/test_views.py
+++ modulo003-python-web/20221112_aula10/mensagens/tests/test_views.py
@@ -31,3 +31,45 @@ class TestMensagemViews(TestCase):
 
         self.assertTemplateUsed("mensagens/index.html")
         self.assertEqual(response.status_code, 200)
+
+    def test_view_nova_mensagem_metodo_get_deve_retornar_sucesso(self):
+        response = self.client.get(reverse("mensagens:nova_mensagem"))
+
+        self.assertTemplateUsed("mensagens/nova_mensagem.html")
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_nova_mensagem_metodo_post_deve_salvar_nova_mensagem(self):
+        response = self.client.post(
+            reverse("mensagens:nova_mensagem"),
+            {
+                "mensagem_titulo": "A Linguagem de Programação Python",
+                "mensagem_corpo": "Python é uma linguagem interpretada",
+                "mensagem_autor": "Guido Von Rossum"
+            }
+        )
+
+        mensagem = Mensagem.objects.filter(autor__startswith="Guido").first()
+
+        self.assertEqual(mensagem.titulo, "A Linguagem de Programação Python")
+        self.assertEqual(mensagem.corpo, "Python é uma linguagem interpretada")
+        self.assertEqual(mensagem.autor, "Guido Von Rossum")
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_view_nova_mensagem_metodo_post_deve_salvar_nova_mensagem_autor_anonimo(self):
+        response = self.client.post(
+            reverse("mensagens:nova_mensagem"),
+            {
+                "mensagem_titulo": "A Linguagem de Programação Python",
+                "mensagem_corpo": "Python é uma linguagem interpretada",
+                "mensagem_autor": ""
+            }
+        )
+
+        mensagem = Mensagem.objects.filter(corpo__startswith="Python").first()
+
+        self.assertEqual(mensagem.titulo, "A Linguagem de Programação Python")
+        self.assertEqual(mensagem.corpo, "Python é uma linguagem interpretada")
+        self.assertEqual(mensagem.autor, "Anônimo")
+
+        self.assertEqual(response.status_code, 302)
